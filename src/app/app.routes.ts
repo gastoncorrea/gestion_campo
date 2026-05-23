d1e2e9da6e66a286f59453bde11d4eb4072f1cbb
unknown <gastoncorrea90e@gmail.com>
2026-05-23 20:41:55 -0300

    home page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { OrdenTrabajoList } from './components/orden-trabajo-list/orden-trabajo-list';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
     {
         path: '',
+        component: Home
+    },
+    {
+        path:'ot',
         component: OrdenTrabajoList
     }
 ];
