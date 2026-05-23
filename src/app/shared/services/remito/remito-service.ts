795cc7dc763b71aa20a6cfaf3e274c45dcb03227
unknown <gastoncorrea90e@gmail.com>
2026-05-23 20:53:28 -0300

    remito service created

--- src/app/shared/services/remito/remito-service.ts
+++ src/app/shared/services/remito/remito-service.ts
@@ -1,6 +1,41 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RemitoService {}
+export class RemitoService {
+  private http = inject(HttpClient);
+
+  private apiKey = environment.apiKey;
+  private spreadsSheetId = environment.spreadsheetId;
+  private sheetName = "Remito";
+
+  obtenerRemitos():Observable<any[]>{
+    const rango = encodeURIComponent(`${this.sheetName}!A1:Z1000`);
+
+    const url =
+    `https://sheets.googleapis.com/v4/spreadsheets/`+
+      `${this.spreadsSheetId}/values/${rango}?key=${this.apiKey}`;
+
+      return this.http.get<any>(url).pipe(
+        map(response => {
+          const rows = response.value || [];
+          if(rows.length === 0) return [];
+
+          const headers = rows[0];
+
+          return rows.slice(1).map((row:any[])=>{
+            const obj:any = {};
+
+            headers.forEach((header:string, index:number) => {
+              obj[header] = row[index]??'';
+            });
+            return obj;
+          })
+        })
+      )
+  }
+}
