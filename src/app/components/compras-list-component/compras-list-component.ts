61128e7325371820c1c01776705cb31fecd5ff46
unknown <gastoncorrea90e@gmail.com>
2026-05-28 21:08:48 -0300

    compras list component created

---
+++ src/app/components/compras-list-component/compras-list-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compras-list-component',
+  imports: [],
+  templateUrl: './compras-list-component.html',
+  styleUrl: './compras-list-component.scss',
+})
+export class ComprasListComponent {}
